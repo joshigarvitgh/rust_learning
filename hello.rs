a9658638ef6437e9cdaa5eabe493d715adfa1c8e
garvit joshi <gajo783a@tu-dresden.de>
2023-08-02 11:55:59 +0200

    first commit

---
+++ hello.rs
@@ -0,0 +1,13 @@
+fn
+main(){
+   println!("Rust says Hello !!");
+   let company_string = "TUD";  // string type
+   let rating_float = 4.5;                 // float type
+   let is_growing_boolean = true;          // boolean type
+   let icon_char = '♥';                    //unicode character type
+
+   println!("company name is:{}",company_string);
+   println!("company rating on 5 is:{}",rating_float);
+   println!("company is growing :{}",is_growing_boolean);
+   println!("company icon is:{}",icon_char);
+}
